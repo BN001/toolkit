8314e3c62be22a65f4a64b77671e03069b74d0e0
BN001 <73430963+BN001@users.noreply.github.com>
2025-09-11 23:12:02 +0500

    Update delete.py

--- routes/v1/s3/delete.py
+++ routes/v1/s3/delete.py
@@ -1,6 +1,7 @@
-import os
 from flask import Blueprint, request, jsonify
-from services.s3_toolkit import delete_from_s3
+import boto3
+from botocore.client import Config
+import os
 
 v1_s3_delete_bp = Blueprint("v1_s3_delete", __name__)
 
@@ -12,16 +13,22 @@ def s3_delete():
         if not file_key:
             return jsonify({"error": "file_key is required"}), 400
 
-        # Используем такие же переменные, как upload/list
-        s3_url = os.getenv("S3_URL")
+        endpoint_url = os.getenv("S3_ENDPOINT_URL")
         access_key = os.getenv("S3_ACCESS_KEY")
         secret_key = os.getenv("S3_SECRET_KEY")
         bucket_name = os.getenv("S3_BUCKET_NAME")
         region = os.getenv("S3_REGION")
 
-        response = delete_from_s3(
-            s3_url, access_key, secret_key, bucket_name, region, file_key
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region,
+            endpoint_url=endpoint_url,
+            config=Config(signature_version="s3v4"),
         )
+
+        response = s3.delete_object(Bucket=bucket_name, Key=file_key)
 
         return jsonify({
             "status": "success",
